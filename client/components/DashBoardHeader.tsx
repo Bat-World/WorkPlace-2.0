8e3072cf39c0d77bdc71fc7c0cd21a8d8c251477
24LP4462 24LP4462 <24LP4462@G304-CC0229.local>
2025-06-23 10:36:42 +0800

    FRONTEND: DashBoard page style

--- client/components/DashBoardHeader.tsx
+++ client/components/DashBoardHeader.tsx
@@ -2,7 +2,7 @@
 
 export const DashBoardHeader = () => {
   return (
-    <header className="bg-black border-b border-gray-700 px-6 py-4 text-white ">
+    <div className="bg-black border-b border-gray-700 px-6 py-4 text-white ">
       <div className="flex items-center justify-between mx-auto max-w-7xl">
         <div className="flex items-center gap-10">
           <p className="text-[24px] font-medium tracking-[-0.03em] leading-8">
@@ -10,26 +10,48 @@ export const DashBoardHeader = () => {
           </p>
 
           <div className="flex items-center gap-2">
-              <img src={"/tinder-logo.svg"} alt="Tinder" className="h-7" />
-              <span className="text-white font-medium">Tinder</span>
-              <svg
-                className="w-4 h-4 text-white ml-2"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7 10l5 5 5-5z" />
-              </svg>
+            <img src={"/tinder-logo.svg"} alt="Tinder" className="h-7" />
+            <span className="text-white font-medium">Tinder</span>
+            <svg
+              className="w-4 h-4 text-white ml-2"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M7 10l5 5 5-5z" />
+            </svg>
           </div>
         </div>
 
-        <div className="flex items-center gap-x-8">
-      {['Нүүр', 'Төслүүд', 'Таскууд', 'Календар'].map((item, idx) => (
-        <div key={idx} className="group flex items-center gap-2 cursor-pointer hover:text-white transition">
-          <img src={`/${item === 'Нүүр' ? 'home' : item === 'Төслүүд' ? 'folder' : item === 'Таскууд' ? 'task' : 'calendar'}.svg`} alt={item} className="h-6" />
-          <p className="text-xl text-[#A5A5A9] group-hover:text-white transition">{item}</p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-10">
+            <div className="group flex items-center gap-2 cursor-pointer hover:text-white transition">
+              <img src="/home.svg" alt="Home" className="h-6" />
+              <p className="text-xl text-[#A5A5A9] group-hover:text-white transition">
+                Нүүр
+              </p>
+            </div>
+
+            <div className="group flex items-center gap-2 cursor-pointer hover:text-white transition">
+              <img src="/folder.svg" alt="Projects" className="h-6" />
+              <p className="text-xl text-[#A5A5A9] group-hover:text-white transition">
+                Төслүүд
+              </p>
+            </div>
+
+            <div className="group flex items-center gap-2 cursor-pointer hover:text-white transition">
+              <img src="/task.svg" alt="Tasks" className="h-6" />
+              <p className="text-xl text-[#A5A5A9] group-hover:text-white transition">
+                Таскууд
+              </p>
+            </div>
+            <div className="group flex items-center gap-2 cursor-pointer hover:text-white transition">
+              <img src="/calendar.svg" alt="Calendar" className="h-6" />
+              <p className="text-xl text-[#A5A5A9] group-hover:text-white transition">
+                Календар
+              </p>
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
 
         <div className="flex items-center gap-3 bg-[#101014] border border-[#26282A] rounded-full px-4 py-2">
           <img
@@ -44,7 +66,7 @@ export const DashBoardHeader = () => {
             </span>
           </div>
           <svg
-            className="w-4 h-4 transform rotate-[-90deg] ml-1"
+            className="w-4 h-4  ml-1"
             fill="currentColor"
             viewBox="0 0 24 24"
           >
@@ -52,6 +74,6 @@ export const DashBoardHeader = () => {
           </svg>
         </div>
       </div>
-    </header>
+    </div>
   );
 };
