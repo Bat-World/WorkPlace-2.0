a7117a4f72b80d4a4502badaa92c9e1c50c38c50
24LP4462 24LP4462 <24LP4462@G304-CC0229.local>
2025-06-23 18:07:30 +0800

    FRONTEND: tasks page

---
+++ client/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+import { Tasks } from "@/components/Tasks";
+
+export default function Task () {
+    return(
+        <div className="bg-black">
+            <Tasks/>
+        </div>
+    )
+}
---
+++ client/components/Tasks.tsx
@@ -0,0 +1,43 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
+import { Button } from "./ui/button";
+import { DialogHeader } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { ProjectTasks } from "./ProjectTasks";
+
+
+export const Tasks = () => {
+  return (
+    <div className="max-w-[1348px] mx-auto px-4 sm:px-6 lg:px-8 mt-[20px]">
+      <div className="flex justify-between items-center h-16">
+          <div className="flex items-center space-x-4">
+            <div className="text-2xl font-bold text-white">Swipe Integrations</div>
+            <div className="text-sm text-gray-500">/ Tasks</div>
+          </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="bg-[#2FC285]">+ Нэмэх</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>New Project</DialogTitle>
+              <DialogDescription>Enter project details below</DialogDescription>
+            </DialogHeader>
+            <div className="space-y-4 mt-4">
+              <Input placeholder="Project Title" />
+              <Input />
+              <Button></Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      <ProjectTasks/>
+    </div>
+  );
+};
